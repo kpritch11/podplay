fb33f6e6bd05d24980dce51d9ac27bcb3cdb8cea
Kurtis Pritchard <kpritch11@gmail.com>
2018-08-21 17:35:40 -0500

    Added search.

--- app/src/main/java/self/edu/kurtis/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/self/edu/kurtis/podplay/ui/PodcastActivity.kt
@@ -2,13 +2,19 @@ package self.edu.kurtis.podplay.ui
 
 import android.app.SearchManager
 import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.support.v7.widget.SearchView
+import android.util.Log
 import self.edu.kurtis.podplay.R
+import self.edu.kurtis.podplay.repository.ItunesRepo
+import self.edu.kurtis.podplay.service.ItunesService
 
 class PodcastActivity : AppCompatActivity() {
+
+    val TAG = javaClass.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,5 +32,26 @@ class PodcastActivity : AppCompatActivity() {
         searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
 
         return true
+    }
+
+    private fun performSearch(term: String) {
+        val itunesService = ItunesService.instance
+        val itunesRepo = ItunesRepo(itunesService)
+        itunesRepo.searchByTerm(term) {
+            Log.i(TAG, "Results = $it")
+        }
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+            performSearch(query)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent)
     }
 }
