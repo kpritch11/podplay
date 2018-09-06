ff473f945dbec737ec8374adabe8c03d583dbd99
Kurtis Pritchard <kpritch11@gmail.com>
2018-09-05 19:17:59 -0500

    Added rss feed service.

--- app/src/main/java/self/edu/kurtis/podplay/repository/PodcastRepo.kt
+++ app/src/main/java/self/edu/kurtis/podplay/repository/PodcastRepo.kt
@@ -1,9 +1,14 @@
 package self.edu.kurtis.podplay.repository
 
 import self.edu.kurtis.podplay.model.Podcast
+import self.edu.kurtis.podplay.service.RssFeedService
 
 class PodcastRepo {
     fun getPodcast(feedUrl: String, callback: (Podcast?) -> Unit) {
+        val rssFeedService = RssFeedService()
+        rssFeedService.getFeed(feedUrl) {
+
+        }
         callback(Podcast(feedUrl, "No Name", "No description", "No image"))
     }
 }
--- app/src/main/java/self/edu/kurtis/podplay/util/DateUtils.kt
+++ app/src/main/java/self/edu/kurtis/podplay/util/DateUtils.kt
@@ -16,4 +16,10 @@ object DateUtils {
 
         return outputFormat.format(date)
     }
+
+    fun xmlDateToDate(date: String?): Date {
+        val date = date ?: return Date()
+        val inFormat = SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z")
+        return inFormat.parse(date)
+    }
 }
