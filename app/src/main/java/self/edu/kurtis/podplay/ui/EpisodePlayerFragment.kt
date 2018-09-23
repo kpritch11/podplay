a4be37a29fcd6523832b3cfb55d63385505f8bec
Kurtis Pritchard <kpritch11@gmail.com>
2018-09-23 10:02:56 -0500

    Setup episode description.

--- app/src/main/java/self/edu/kurtis/podplay/ui/EpisodePlayerFragment.kt
+++ app/src/main/java/self/edu/kurtis/podplay/ui/EpisodePlayerFragment.kt
@@ -1,13 +1,21 @@
 package self.edu.kurtis.podplay.ui
 
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_episode_player.*
 import self.edu.kurtis.podplay.R
+import self.edu.kurtis.podplay.util.HtmlUtils
+import self.edu.kurtis.podplay.viewmodel.PodcastViewModel
 
 class EpisodePlayerFragment: Fragment() {
+    private lateinit var podcastViewModel: PodcastViewModel
+
     companion object {
         fun newInstance(): EpisodePlayerFragment {
             return EpisodePlayerFragment()
@@ -17,6 +25,7 @@ class EpisodePlayerFragment: Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         retainInstance = true
+        setupViewModel()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -25,6 +34,7 @@ class EpisodePlayerFragment: Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        updateControls()
     }
 
     override fun onStart() {
@@ -33,5 +43,21 @@ class EpisodePlayerFragment: Fragment() {
 
     override fun onStop() {
         super.onStop()
+    }
+
+    private fun setupViewModel() {
+        podcastViewModel = ViewModelProviders.of(activity).get(PodcastViewModel::class.java)
+    }
+
+    private fun updateControls() {
+        episodeTitleTextView.text = podcastViewModel.activeEpisodeViewData?.title
+
+        val htmlDesc = podcastViewModel.activeEpisodeViewData?.description ?: ""
+        val descSpan = HtmlUtils.htmlToSpannable(htmlDesc)
+        episodeDescTextView.text = descSpan
+        episodeDescTextView.movementMethod = ScrollingMovementMethod()
+
+        Glide.with(activity).load(podcastViewModel.activePodcastViewData?.imageUrl).into(episodeImageView)
+
     }
 }
